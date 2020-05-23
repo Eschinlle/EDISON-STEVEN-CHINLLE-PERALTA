516d5eddfd94c032835d9a7d9772564ecd103f76
Eschinlle <65572806+Eschinlle@users.noreply.github.com>
2020-05-23 15:21:23 -0500

    Add files via upload
    PROGRAMA HOLA MUNDO_ACTIVIDAD DE APRENDIZAJE 1_POO

---
+++ HolaMundo.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    system("Color F1");
+    cout<<"ACTIVIDAD DE APRENDIZAJE NUMERO 1"<<endl<<endl;
+    cout<<"Hola Mundo, PROGRAMACION ORIENTADA A OBJETOS..."<<endl<<endl;
+    system("Pause");
+    return 0;
+}
